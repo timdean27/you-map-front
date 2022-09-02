e7830888e8b92948724480aeaddc80ee7b4acb3f
timdean27 <timdean27@yahoo.com>
2022-09-02 12:43:20 -0400

    Completed Sign up page using Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router } from "react-router-dom";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div>
-    Hello local reviews front
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Signup />
+      </div>
+    </Container>
   );
 }
 
