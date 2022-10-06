ccbfddf6b5544b911198888f05c75f48088c6e5a
timdean27 <timdean27@yahoo.com>
2022-10-06 13:31:14 -0400

    restarting

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from "react"
-
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "./contexts/AuthContext"
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom"
 
-import Signup from "./Auth_components/Signup"
-import Dashboard from "./Auth_components/Dashboard"
-import Login from "./Auth_components/Login"
+
 
 function App() {
   return (
-    <Container
-    className="d-flex align-items-center justify-content-center"
-    style={{ minHeight: "100vh" }}
-  >
-    <div className="w-100" style={{ maxWidth: "400px" }}>
-      <Router>
-        <AuthProvider>
-            <Routes>
-            <Route exact path="/" element={<Dashboard/>} />
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/login" element={<Login/>}/>
-            </Routes>
-        </AuthProvider>
-      </Router>
+
+    <div>
+Hello Maps
+
     </div>
-  </Container>
+
   );
 }
 
