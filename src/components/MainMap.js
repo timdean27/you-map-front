bcafc8a2fa38db6791488d3b9390d1ce1a75218b
vrkljam <vrkljam@earthlink.net>
2023-01-06 12:29:26 -0700

    map is visible on page

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -4,8 +4,8 @@ import mapboxgl from 'mapbox-gl';
 
 
 // Mapbox key
-const T = process.env.REACT_APP_TOKEN
-mapboxgl.accessToken=T
+const MapboxToken = process.env.REACT_APP_TOKEN
+mapboxgl.accessToken=MapboxToken
 
 function MainMap() {
 
@@ -13,37 +13,37 @@ function MainMap() {
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [zoom, setZoom] = useState(5);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
 
     useEffect(() => {
             // from Mapbox.com
-        const map = new mapboxgl.Map({
+        if (map.current) return;
+        map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v12',
-            center: [-122.26,37.79],
+            center: [lng, lat],
             zoom: zoom,
             });
-         
             // from Mapbox.com
-        //     map((marker) => {
-        //     new mapboxgl.Marker()
-        //     .setLngLat(marker)
-        //     .setPopup(new mapboxgl.Popup({offset:50})
-        //     // looking to populate shelter name in line below
-        //     .setHTML(`${marker.name}`))
-        //     .addTo(map)
-        // });
+            // const marker = new mapboxgl.Marker()
+            // .setLngLat(-122.26,37.79)
+            // .addTo(map)
+        });
+         
+   
+        useEffect(() => {
+            if (!map.current) return; // wait for map to initialize
+            map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+                });
+            });
 
             // flyThis()
-        }, []);
+       
 
-        // useEffect(() => {
-        //     if (!map.current) return; // wait for map to initialize
-        //     map.current.on('move', () => {
-        //     setLng(map.current.getCenter().lng.toFixed(4));
-        //     setLat(map.current.getCenter().lat.toFixed(4));
-        //     setZoom(map.current.getZoom().toFixed(2));
-        //     });
-        //  });
 
         //  function flyThis () {map.flyTo({
         //     center: [(Math.random() - 0.5) * 360, (Math.random() - 0.5) * 100],
@@ -54,6 +54,9 @@ function MainMap() {
   return (
     <div>
         {/* Gives the map with the inital coordinates */}
+        <div className="sidebar">
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
         <div ref={mapContainer} className="map-container mt-5" />
     </div>
   )
