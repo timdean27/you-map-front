06dd5af4390e867da60f863245c00d774342d9c4
vrkljam <vrkljam@earthlink.net>
2023-01-27 15:34:22 -0700

    added posts part

--- src/components/rightside/Rightside.jsx
+++ src/components/rightside/Rightside.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Post from '../post/Post'
 import Share from '../share/Share'
 import './rightside.css'
 
@@ -7,6 +8,16 @@ function Rightside() {
     <div className='rightside'> Rightside
         <div className="rightside-wrapper">
             <Share />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
         </div>
     </div>
   )
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './share.css'
 import { GoFileMedia } from 'react-icons/go';
+import { MdFavoriteBorder } from 'react-icons/md';
+import { AiOutlineLike } from 'react-icons/ai';
+
+
 
 function Share() {
   return (
@@ -15,10 +19,23 @@ function Share() {
             <div className="share-bottom">
                 <div className="share-options">
                     <div className="share-option">
-                        <GoFileMedia className='share-icon'/>
+                        <GoFileMedia color="blue" className='share-icon'/>
+                        <span className='share-option-text'>photo or video </span>
+                    </div>
+                    <div className="share-option">
+                        <MdFavoriteBorder color="green" className='share-icon'/>
+                        <span className='share-option-text'>Favorite </span>
+                    </div>
+                    <div className="share-option">
+                        <AiOutlineLike color="red" className='share-icon'/>
+                        <span className='share-option-text'>Like </span>
+                    </div>
+                    <div className="share-option">
+                        <GoFileMedia color='orange' className='share-icon'/>
                         <span className='share-option-text'>photo or video </span>
                     </div>
                 </div>
+                <button className="share-btn">Share</button>
             </div>
         </div>
     </div>
