ac70ab5581eeaa398a78f8dbc8a856028697a1f2
timdean27 <timdean27@yahoo.com>
2023-02-03 10:19:05 -0500

    changes to map, added popUP folder for edit and post

---
+++ src/components/PopUP/EditPopUpInfo.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useCallback } from "react";
+import Dropzone from "react-dropzone";
+import { useDropzone } from "react-dropzone";
+
+const EditPopUpInfo = ({ selectedMarker , locationsMarked , setLocationsMarked, setEditPopupInfo}) => {
+  const [title, setTitle] = useState("Title");
+  let locationWeWantIndex = locationsMarked.findIndex(locationWeWant => locationWeWant.coordinates=== selectedMarker.coordinates)
+  console.log("Index of the location we want in edit info page", locationWeWantIndex)
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    console.log("A name was submitted: ");
+    e.preventDefault();
+    setEditPopupInfo(false)
+  };
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsArrayBuffer(file);
+      console.log(file);
+    });
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+
+  useEffect(() => {
+    
+  }, []);
+
+
+  return (
+
+        <div>
+          Model is Open
+          <form onSubmit={handleSubmit}>
+            <label>
+              Name:
+              <input
+                id="Title"
+                type="text"
+                placeholder={title}
+                onChange={handleChange}
+              />
+            </label>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+
+  );
+};
+
+export default EditPopUpInfo;
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,16 +1,11 @@
 import React, { useState, useEffect } from "react";
-import './home.css'
+import ReactMapGL, { Marker, Popup, NavigationControl } from "react-map-gl";
 
-import ReactMapGL, {
-  Marker,
-  Popup,
-  GeolocateControl,
-  NavigationControl,
-} from "react-map-gl";
-
-import PopUpInfo from "../components/PopUpInfo";
-
+import EditPopUpInfo from "../components/PopUP/EditPopUpInfo";
+import ViewPopUpStart from "../components/PopUP/ViewPopUpStart";
+import ChatInPopUp from "../components/PopUP/ChatInPopUp";
 function MapPage() {
+  const [editPopupInfo, setEditPopupInfo] = useState(false);
   const [selectedMarker, setSelectedMarker] = useState(null);
   const [currentLatLng, setCurrentLatLng] = useState(["0.00000", "0.00000"]);
   const [viewState, setViewState] = useState({
@@ -23,25 +18,35 @@ function MapPage() {
     minZoom: 0,
     projection: "globe",
   });
-
   const [locationsMarked, setLocationsMarked] = useState([
     {
       name: "New York",
       coordinates: [-73.33, 40.87], // [long , lat]
+      images: [
+        "https://cdn.contexttravel.com/image/upload/c_fill,q_60,w_2600/v1549321174/production/city/hero_image_12_1549321174.jpg",
+        "https://img.grouponcdn.com/deal/NHjGV2FkqS3qKFu3wqWwP4M3xB6/NH-976x586/v1/c870x524.jpg",
+        "https://media.gettyimages.com/id/1269226133/photo/concept-of-new-york-city-statue-of-liberty-downtown.jpg?s=612x612&w=gi&k=20&c=j490kQvLK__AlFHjeip2eyt9X954Nda82kn1yIs3vkk=",
+        "https://media.istockphoto.com/id/1340381886/photo/aerial-view-of-manhattan-at-sunset.jpg?b=1&s=170667a&w=0&k=20&c=uXyWuZuP-4maEheV1sc90cmwBGG4fn3C108_MgJ9Zro=",
+      ],
     },
     {
       name: "Denver",
       coordinates: [-105.33, 39.87],
+      images: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQexzIiOxb447izZqKuAndd_bSD8EX146wzTIx7K6uiiPPqsqkKvr4E0xUbj8KasgVF5AM&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAeYzcZ0kyg59IbU6uJnC0vBu_8UG1zvB-yaSsIbcVUJWNHaZKdPaWWUD5alv5lBtDgok&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu_VFA4Al87aQBYSClO7TT3IGZoKXN-FVI8g&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6r9FJ03CZaE9cXY3Fs2IXMdKewUk3glFMTQ&usqp=CAU",
+      ],
     },
   ]);
 
-
-// On move fucntionality sets current lat and long (print e to see full object)
-// style and global are hard set in ReactMapGl comp
+  // On move fucntionality sets current lat and long (print e to see full object)
+  // style and global are hard set in ReactMapGl comp
   const onMoveFunc = (e) => {
-//    console.log("event from move fucntion",e);
+    //    console.log("event from move fucntion",e);
     setViewState(e.viewState);
-    setCurrentLatLng([e.viewState.latitude , e.viewState.longitude])
+    setCurrentLatLng([e.viewState.latitude, e.viewState.longitude]);
   };
 
   const getLocation = (event) => {
@@ -62,22 +67,20 @@ function MapPage() {
     ]);
   };
 
-  
   const clickFuncSelectMarker = (markedLocation) => {
-    if(markedLocation != selectedMarker){
-    console.log("marked location in onClick button",markedLocation);
-    setSelectedMarker(markedLocation);
-    }
-    else(console.log(markedLocation))
-  }
+    if (markedLocation != selectedMarker) {
+      console.log("marked location in onClick button", markedLocation);
+      setSelectedMarker(markedLocation);
+    } else console.log(markedLocation);
+  };
 
-  useEffect(() => {
-  }, []);
-  
-  console.log("selected marker",selectedMarker);
+  useEffect(() => {}, []);
+
+  // console.log("selected marker",selectedMarker);
+  // function to Edit Popup
 
   return (
-    <div className="mappage">
+    <div>
       <div className="CurrentLatLngBox">
         Current Location
         <div>Latitude:{currentLatLng[0]}</div>
@@ -95,9 +98,6 @@ function MapPage() {
           getLocation(event);
         }}
       >
-        {/* GeolocateControl imports button that will send us to current location*/}
-        {/* <GeolocateControl ref={geolocateControlRef}></GeolocateControl> */}
-        {/* NavigationControl gives us zoom and move buttons on side of map */}
         <NavigationControl />
         {locationsMarked
           ? locationsMarked.map((markedLocation, index) => (
@@ -106,38 +106,58 @@ function MapPage() {
                 latitude={markedLocation.coordinates[1]}
                 longitude={markedLocation.coordinates[0]}
               >
-                <button
-                  onClick={e =>{
+                <div
+                  className="marker"
+                  onClick={(e) => {
                     e.preventDefault();
                     clickFuncSelectMarker(markedLocation);
                   }}
                 >
                   {markedLocation.name}
-                </button>
+                </div>
               </Marker>
             ))
           : null}
 
         {selectedMarker ? (
-          <div>
-         {console.log("this should print when selectedMarker is true",selectedMarker, )}
-            <Popup
-              closeOnClick={false}
-              closeButton={true}
-              latitude={selectedMarker.coordinates[1]}
-              longitude={selectedMarker.coordinates[0]}
-              onClose={e => {
-              setSelectedMarker(null)}}
-            >
+          <Popup
+            closeOnClick={false}
+            closeButton={true}
+            latitude={selectedMarker.coordinates[1]}
+            longitude={selectedMarker.coordinates[0]}
+            onClose={(e) => {
+              setSelectedMarker(null);
+              setEditPopupInfo(false);
+            }}
+          >
+            {editPopupInfo ? (
               <div>
-              <PopUpInfo />
+                <EditPopUpInfo
+                  selectedMarker={selectedMarker}
+                  locationsMarked={locationsMarked}
+                  setLocationsMarked={setLocationsMarked}
+                  setEditPopupInfo={setEditPopupInfo}
+                />
               </div>
-            </Popup>
-            </div>
-        ) : console.log("failing at Popup")}
+            ) : (
+              <div>
+                <button
+                  onClick={(e) => {
+                    setEditPopupInfo(true);
+                  }}
+                >
+                  Edit
+                </button>
+                <ViewPopUpStart selectedMarker={selectedMarker} />
+                <ChatInPopUp />
+              </div>
+            )}
+          </Popup>
+        ) : null}
       </ReactMapGL>
     </div>
   );
 }
 export default MapPage;
+
 
